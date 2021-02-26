f8e7a325b4bd0e7bc4db65dfa766acb3455790ab
Matt Dainty <matt@bodgit-n-scarper.com>
2021-02-26 15:09:36 +0000

    Refactor to use PDK
    Fix Puppet versions and dependencies.
    
    Fixes #4

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,81 +1,29 @@
-# == Class: watchdog
+# Installs and manages a watchdog.
 #
-# This class manages a hardware watchdog.
+# @example Declaring the class
+#   include ::watchdog
 #
-# === Parameters
-#
-# [*package_ensure*]
-#   Intended state of the package providing the watchdog daemon.
-#   Default: installed
-#
-# [*package_name*]
-#   The package name that provides the watchdog daemon.
-#   Default: watchdog
-#
-# [*period*]
-#   The watchdog timeout period.
-#   Default: 60
-#
-# [*service_enable*]
-#   Whether to enable the watchdog service.
-#   Default: true
-#
-# [*service_ensure*]
-#   Intended state of the watchdog service.
-#   Default: running
-#
-# [*service_manage*]
-#   Whether to manage the watchdog service or not.
-#   Default: true
-#
-# [*service_name*]
-#   The name of the watchdog service.
-#   Default: watchdog
-#
-# === Variables
-#
-# None.
-#
-# === Examples
-#
-#  class { 'watchdog': }
-#
-# === Authors
-#
-# Matt Dainty <matt@bodgit-n-scarper.com>
-#
-# === Copyright
-#
-# Copyright 2014 Matt Dainty, unless otherwise noted.
-#
+# @param manage_package
+# @param package_name The name of the package.
+# @param period
+# @param service_enable
+# @param service_ensure
+# @param service_manage Whether to manage the service.
+# @param service_name Name of the service.
 class watchdog (
-  $manage_package = $::watchdog::params::manage_package,
-  $package_ensure = $::watchdog::params::package_ensure,
-  $package_name   = $::watchdog::params::package_name,
-  $period         = $::watchdog::params::period,
-  $service_enable = $::watchdog::params::service_enable,
-  $service_ensure = $::watchdog::params::service_ensure,
-  $service_manage = $::watchdog::params::service_manage,
-  $service_name   = $::watchdog::params::service_name
-) inherits ::watchdog::params {
+  Boolean                 $manage_package,
+  Optional[String]        $package_name,
+  Integer[0]              $period,
+  Boolean                 $service_enable,
+  Stdlib::Ensure::Service $service_ensure,
+  Boolean                 $service_manage,
+  String                  $service_name,
+) {
 
-  validate_bool($manage_package)
-  validate_string($package_ensure)
-  validate_string($package_name)
-  validate_integer($period)
-  validate_bool($service_enable)
-  validate_re($service_ensure, '^(running|stopped)$')
-  validate_bool($service_manage)
-  validate_string($service_name)
+  contain watchdog::install
+  contain watchdog::config
+  contain watchdog::service
 
-  include ::watchdog::install
-  include ::watchdog::config
-  include ::watchdog::service
-
-  anchor { 'watchdog::begin': }
-  anchor { 'watchdog::end': }
-
-  Anchor['watchdog::begin'] -> Class['::watchdog::install']
-    ~> Class['::watchdog::config'] ~> Class['::watchdog::service']
-    -> Anchor['watchdog::end']
+  Class['watchdog::install'] -> Class['watchdog::config']
+    ~> Class['watchdog::service']
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,9 +1,9 @@
-#
+# @!visibility private
 class watchdog::install {
 
-  if $::watchdog::manage_package {
-    package { $::watchdog::package_name:
-      ensure => $::watchdog::package_ensure,
+  if $watchdog::manage_package {
+    package { $watchdog::package_name:
+      ensure => present,
     }
   }
 }
